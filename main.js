4ebbf9910bb2d59c8d0623ed7b1fc02c7dd9f0f1
Jamal Mohamed Ameer <jamalbe2004@gmail.com>
2023-09-12 21:54:25 +0300

    slider section added

--- main.js
+++ main.js
@@ -10,3 +10,31 @@ import viteLogo from '/vite.svg'
 import Alpine from 'alpinejs'
 window.Alpine = Alpine
 Alpine.start()
+
+
+//GlideJS slider
+import Glide from '@glidejs/glide'
+
+
+var glidePartners = new Glide('.glide-partners', {
+  type: 'carousel',
+  perView: 5,
+  autoplay: 1000,
+  animationDuration: 3000,
+  gap: 80,
+  breakpoints: {
+      1024: {
+          perView: 3,
+          gap: 40
+      },
+      640: {
+          perView: 2,
+          gap: 24
+      },
+      480: {
+          perView: 1
+      }
+  },
+});
+
+glidePartners.mount();
